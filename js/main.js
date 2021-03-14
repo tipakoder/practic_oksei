884cb4f4dade385f63a2f165e974d72a04044cfa
Tipa Koder <44733288+tipakoder@users.noreply.github.com>
2021-03-14 13:14:48 +0500

    Некоторые изменения
    - Добавлено 2 новых карусели, отображающие разный номер
    - Загрузка вложений и аватарок
    - Включена функция получения GET-параметров
    - Отлажено отсекание картинок, которые отсутствуют

--- js/main.js
+++ js/main.js
@@ -8,16 +8,16 @@ var debug = true;
 // Настройки
 var carouselDurationUpdate = 10000; // seconds
 var carouselDurationAnimation = 1000; // seconds
+var carouselDefaultImage = "img/cover.png";
 
 // При старте
 function ready(){
 	// Включаем дэбаг мод
 	if(debug){
-		// carouselDurationUpdate = 6000;
 		// Добавляем тестовый вариант карусели
-		let carouselId = addCarouselTiter('79647090506');
-		titresList[carouselId].addMessage({title: "hi!", text: "text"});
-		titresList[carouselId].addMessage({title: "hi111!", text: "text1"});
+		addCarouselTiter('79647090506');
+		addCarouselTiter('79619002008', "carousel1");
+		addCarouselTiter('79325503338', "carousel2");
 	}
 
 	// Стартуем интервал жизни цикла
@@ -40,7 +40,7 @@ function addCarouselTiter(user, idTitre = "carousel"){
 	titresList.push({
 		type: "carousel",
 		messages: [],
-		user: 0,
+		user: user,
 		lastMessageId: 0,
 		nextMessageIndex: 0,
 		isStarted: false,
@@ -57,28 +57,68 @@ function addCarouselTiter(user, idTitre = "carousel"){
 					alert("Неверный ответ от сервера");
 				}
 			}).then((data) => {
-				console.log(data);
+				// Отладка
+				if(debug) console.log(data);
+				// Добавляем сообщения в общий массив
+				for(let message of data.messagesList){
+					if(data.deletedMessagesIdList.indexOf(message.id) == -1) {
+						this.addMessage(message);
+					}
+				}
+				// Запоминаем индекс последнего сообщения
+				this.lastMessageId = this.messages[0].id;
 			}).catch((error) => {
 				console.log(error);
 			});
 		},
 		life: function(){
+			// Делаем ссылку на титр
+			let titreObject = document.getElementById(this.idTitre);
 			// Скрываем старое сообщение	
-			if(document.getElementById(this.idTitre).classList.contains("anim-carousel-show")){
-				document.getElementById(this.idTitre).classList.remove("anim-carousel-show");
-				document.getElementById(this.idTitre).classList.add("anim-carousel-hide");
+			if(titreObject.classList.contains("anim-carousel-show")){
+				titreObject.classList.remove("anim-carousel-show");
+				titreObject.classList.add("anim-carousel-hide");
 			}
 			// Убираем анимацию скрытия и показываем
 			setTimeout(() => {
 				// Берём следующее сообщение
 				let currentMessage = this.messages[this.nextMessageIndex];
-				this.nextMessageIndex = ( (this.nextMessageIndex+1) >= this.messages.length) ? 0 : this.nextMessageIndex+1;
+				// Отладка
+				if(debug) console.log(currentMessage);
+				// Отбиваем брак аватарки
+				titreObject.querySelector(".icon").src = carouselDefaultImage;
+				if(currentMessage.message.image != ""){
+					verifyImage(currentMessage.message.image, function(){
+						titreObject.querySelector(".icon").src = this.src;
+					});
+				}
 				// Присваивем значения HTML
-				document.getElementById(this.idTitre).querySelector(".title").textContent = currentMessage.title;
-				document.getElementById(this.idTitre).querySelector(".text").textContent = currentMessage.text;
-				document.getElementById(this.idTitre).classList.remove("anim-carousel-hide"); 
-				document.getElementById(this.idTitre).classList.add("anim-carousel-show");
+				titreObject.querySelector(".title").textContent = currentMessage.message.author;
+				titreObject.querySelector(".text").textContent = currentMessage.message.content;
+				titreObject.querySelector(".attachment").style.display = "none";
+				// Проверяем вложения
+				if(currentMessage.message.attachments.length > 0 && currentMessage.message.attachments[0].type == "image"){
+					// Отсекаем брак фотки
+					verifyImage(currentMessage.message.attachments[0].url, function(){
+						titreObject.querySelector(".attachment").src = this.src;
+						titreObject.querySelector(".attachment").style.display = "block";
+					});
+				}
+				// Присваиваем анимацию появления
+				titreObject.classList.remove("anim-carousel-hide"); 
+				titreObject.classList.add("anim-carousel-show");
+				// Загужаем ID следующего сообщения
+				this.nextMessage();
 			}, carouselDurationAnimation + 500);
+		},
+		nextMessage: function(){
+			let choice = false;
+			while(!choice) {
+				this.nextMessageIndex = ( (this.nextMessageIndex+1) >= this.messages.length) ? 0 : this.nextMessageIndex+1;
+				if(this.messages[this.nextMessageIndex].message.image != ""){
+					choice = true;
+				}
+			}
 		}
 	});
 	// Выводим ID нового титра
@@ -95,6 +135,20 @@ function titresLife(){
 				break;
 		}
 	}
+}
+
+// Подтверждение наличия картинки
+function verifyImage(url, func){
+	let img = new Image();
+	img.src = url;
+	img.onload = func;
+}
+
+// Получение GET параметров
+function get(name){ // получение параметров get
+    if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+        return decodeURIComponent(name[1]);
+    else return false;
 }
 
 // Задаём события
